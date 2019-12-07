31cd9c9281862c20e3d0838a680e257efd101719
Krzysztof Zarębski <56186592+Krzysztof148@users.noreply.github.com>
2019-12-07 12:32:32 +0100

    druga zmiana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nazwisko: ");
         nazwisko = scanner.next();
-        
+        public static void figura() {
+            for(int j=0;j<10;j++) {
+                for (int i=0;i<10;i++);
+                System.out.println("*");
+                System.out.println("");
     public static void main(String[] args) {
 	System.out.println(HelloWorld);
     }
