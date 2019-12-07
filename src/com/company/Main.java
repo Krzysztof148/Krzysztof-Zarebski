1fb354e3af6fec72eadbf102a4c0060e639459f8
Krzysztof Zarębski <56186592+Krzysztof148@users.noreply.github.com>
2019-12-07 12:24:18 +0100

    pierwsze repozytorium

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static void wyswietl() {
+        String nazwisko;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nazwisko: ");
+        nazwisko = scanner.next();
+        
+    public static void main(String[] args) {
+	System.out.println(HelloWorld);
+    }
+}
